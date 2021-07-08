4e760daf8e717e3a141942045d5b7171b1442d59
Maciek Jurczyk <thelairmail@gmail.com>
2021-07-08 05:17:31 +0200

    Build docs

---
+++ precache-manifest.446c414b3a1c9029afcaa83b2ec43578.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6882fd1a2b675d99d01481a26fb80c25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b9a25234048afb4cd31",
+    "url": "/static/js/2.abcdf342.chunk.js"
+  },
+  {
+    "revision": "b4bb354a989475498781f1d72a51a1be",
+    "url": "/static/js/2.abcdf342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d6cf1e3c6530083c851",
+    "url": "/static/js/main.c0bdc2d9.chunk.js"
+  },
+  {
+    "revision": "0ff332f25930b193dbbf",
+    "url": "/static/js/runtime-main.eb6b8f93.js"
+  },
+  {
+    "revision": "e1ab850e2b2f36ff8b99cb43715c51b4",
+    "url": "/static/media/character.e1ab850e.glb"
+  },
+  {
+    "revision": "6235c1a6a3eb340c028629b968a6d191",
+    "url": "/static/media/demo-actions-and-ui.6235c1a6.glb"
+  },
+  {
+    "revision": "fbbe52fadc9e475680e1201a2bef2e90",
+    "url": "/static/media/demo-basic.fbbe52fa.glb"
+  },
+  {
+    "revision": "7e1b2b59056d7a296a615a3f7593b403",
+    "url": "/static/media/demo-game-objects.7e1b2b59.glb"
+  },
+  {
+    "revision": "146d35905c13771c9430e8bbf2c6076b",
+    "url": "/static/media/demo-persistence.146d3590.glb"
+  },
+  {
+    "revision": "79823df6886dfe28ca8d4c9484450674",
+    "url": "/static/media/demo-physics.79823df6.glb"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/font.3ed9575d.ttf"
+  },
+  {
+    "revision": "56f385ddd84e5574b2b827caac2fae6a",
+    "url": "/static/media/intro.56f385dd.glb"
+  },
+  {
+    "revision": "9d266d9a5171ed43120cc3dfe843099f",
+    "url": "/static/media/spinner-default.9d266d9a.png"
+  }
+]);
