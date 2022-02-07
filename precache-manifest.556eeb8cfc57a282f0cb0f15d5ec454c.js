735b3d1468f509d0ac630c297c110efdec9adc73
mjurczyk <thelairmail@gmail.com>
2022-02-07 18:19:34 +0100

    Add chess example to docs

---
+++ precache-manifest.556eeb8cfc57a282f0cb0f15d5ec454c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa6c1fbb814a58e6a315b4b8b22b8cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76ab169ddbd13799934d",
+    "url": "/static/js/2.2c75e1d1.chunk.js"
+  },
+  {
+    "revision": "b4bb354a989475498781f1d72a51a1be",
+    "url": "/static/js/2.2c75e1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb05ef5cf4f4f009607",
+    "url": "/static/js/main.4a1283c9.chunk.js"
+  },
+  {
+    "revision": "0ff332f25930b193dbbf",
+    "url": "/static/js/runtime-main.eb6b8f93.js"
+  },
+  {
+    "revision": "e1ab850e2b2f36ff8b99cb43715c51b4",
+    "url": "/static/media/character.e1ab850e.glb"
+  },
+  {
+    "revision": "6235c1a6a3eb340c028629b968a6d191",
+    "url": "/static/media/demo-actions-and-ui.6235c1a6.glb"
+  },
+  {
+    "revision": "ed8ca94085db88cf55e2a6f7d4be83c1",
+    "url": "/static/media/demo-basic.ed8ca940.glb"
+  },
+  {
+    "revision": "7e1b2b59056d7a296a615a3f7593b403",
+    "url": "/static/media/demo-game-objects.7e1b2b59.glb"
+  },
+  {
+    "revision": "146d35905c13771c9430e8bbf2c6076b",
+    "url": "/static/media/demo-persistence.146d3590.glb"
+  },
+  {
+    "revision": "79823df6886dfe28ca8d4c9484450674",
+    "url": "/static/media/demo-physics.79823df6.glb"
+  },
+  {
+    "revision": "3e84587de6fe71cf983db3746b5cc785",
+    "url": "/static/media/demo-ui-alignment.3e84587d.glb"
+  },
+  {
+    "revision": "889371c79b6d9fec338bf802cecef169",
+    "url": "/static/media/example-chess-board.889371c7.glb"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/font.3ed9575d.ttf"
+  },
+  {
+    "revision": "56f385ddd84e5574b2b827caac2fae6a",
+    "url": "/static/media/intro.56f385dd.glb"
+  },
+  {
+    "revision": "dd7d9e206b0b1843c01862ff2686bd6f",
+    "url": "/static/media/spaichingen_hill_1k.dd7d9e20.hdr"
+  },
+  {
+    "revision": "9d266d9a5171ed43120cc3dfe843099f",
+    "url": "/static/media/spinner-default.9d266d9a.png"
+  }
+]);
